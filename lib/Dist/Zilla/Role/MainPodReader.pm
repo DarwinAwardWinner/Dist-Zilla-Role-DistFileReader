be8c1623190527b6dd661ae12457bc2f98c3b0ec
Ryan C. Thompson <rct@thompsonclan.org>
2014-08-24 07:54:09 -0700

    initial commit

---
+++ lib/Dist/Zilla/Role/MainPodReader.pm
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+package Dist::Zilla::Role::MainPodReader;
+# ABSTRACT: [One-line description of module]
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Role::MainPodReader;
+    [Example code]
+
+=head1 DESCRIPTION
+
+[Description for C<Dist::Zilla::Role::MainPodReader>.]
+
+=head1 BUGS AND LIMITATIONS
+
+Please report any bugs or feature requests to
+C<rct+perlbug@thompsonclan.org>.
+
+=head1 SEE ALSO
+
+=for :list
+
+* [L<Some::Related::Module>]
+
+  [Description of how this module is related]
