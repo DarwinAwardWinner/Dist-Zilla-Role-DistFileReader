981d5c929963fabbb3edcdafa54af6e1ef3ae3c2
Ryan C. Thompson <rct@thompsonclan.org>
2014-08-24 18:05:49 -0700

    Refactor MainModuleReader out of MainPodReader

--- lib/Dist/Zilla/Role/MainPodReader.pm
+++ lib/Dist/Zilla/Role/MainPodReader.pm
@@ -1,7 +1,37 @@
 use strict;
 use warnings;
 package Dist::Zilla::Role::MainPodReader;
-# ABSTRACT: [One-line description of module]
+# ABSTRACT: Something that reads the main module's POD
+
+use Moose::Role;
+with 'Dist::Zilla::Role::MainModuleReader';
+
+# Override default source file to prefer the pod file with the same
+# basename if it exists.
+around '_default_source_file' => sub {
+    my $orig = shift;
+    my $self = shift;
+    my $pm = $self->orig();
+    (my $pod = $pm) =~  s/\.pm$/\.pod/;
+    return -e $pod ? $pod : $pm;
+}
+
+sub _get_source_pod {
+    my ($self) = shift;
+    my $source_content = $self->_get_source_content();
+
+    require PPI::Document;
+
+    my $doc = PPI::Document->new(\$source_content);
+    my $pod_elems = $doc->find('PPI::Token::Pod');
+    my $pod_content = "";
+    if ($pod_elems) {
+        # Concatenation should stringify it
+        $pod_content .= PPI::Token::Pod->merge(@$pod_elems);
+    }
+
+    return $pod_content;
+}
 
 1;
 __END__
